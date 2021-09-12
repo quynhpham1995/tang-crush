cc72b103b8bfd2d23d15d9793af2495884814953
quynhpham1995 <90567919+quynhpham1995@users.noreply.github.com>
2021-09-13 00:58:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,17 @@
 const CONFIG = {
     introTitle: 'Em bé Unicorn ơi! =))',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim chị lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Gặp gỡ nhau muộn chút vẫn tốt chứ sao.
+    Chị đúng độ vào quãng đời chín chắn.
+    Còn em thì bước sang tuổi đủ dịu dàng`,
     btnIntro: 'You ready? =))',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Tất nhiên rồi Quìn còn phải hỏi',
+    btnYes: 'Tất nhiên rồi',
     btnNo: 'Quìn mơ đấy à...',
     question:'Thế giới có hơn 7 tỉ người mà sao em lại nhìn trúng chị thế <3',
     btnReply: 'Gửi lý do cho chị biết vớiiii',
     reply: 'Yêu thì yêu mà không yêu thì yêu <3',
-    mess: 'Chị biết mà 🥰 Nếu ko phải em thì sẽ là ai được nữa',
+    mess: 'Chị biết mà 🥰 Nếu ko phải em thì sẽ là ai được đây',
     messDesc: 'Would you date me after social distancing?',
     btnAccept: 'Em chờ Quìn hỏi mãiii',
     messLink: 'https://www.facebook.com/mirandaxuanquynh/' //link mess của các bạn. VD: https://m.me/nam.nodemy
