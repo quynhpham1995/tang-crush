84416230b6c2dd12708ab7ab48f7c9be282bd7c9
quynhpham1995 <90567919+quynhpham1995@users.noreply.github.com>
2021-09-12 23:31:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Em bé Unicorn ơi! =))',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Lạc vào tim chị lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    btnIntro: 'U ready? =))',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    btnYes: 'Tất nhiên rồi Quìn còn phải hỏi',
+    btnNo: 'Quìn mơ đấy à...',
+    question:'Thế giới có hơn 7 tỉ người mà sao em lại nhìn trúng chị thế <3',
+    btnReply: 'Gửi lý do cho chị biết vớiiii',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <3',
+    mess: 'Chị biết mà 🥰 Nếu ko phải em thì sẽ là ai được nữa',
+    messDesc: 'Would you date me after social distancing?',
+    btnAccept: 'Em chờ Quìn hỏi mãiii',
+    messLink: 'https://www.facebook.com/mirandaxuanquynh/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
