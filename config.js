08b8fbfd7d14dd3d81d3ebd53bf825e7b927ba57
quynhpham1995 <90567919+quynhpham1995@users.noreply.github.com>
2021-09-16 20:03:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const CONFIG = {
     desc: 'Thế mình yêu nhau đi và đừng lo gì cả nhé',
     btnYes: 'Em vẫn đợi Quỳnh hỏi đấy',
     btnNo: 'Quỳnh mơ đấy à',
-    question:'Đùa tí. Nhưng chị thích em là thật',
+    question:'Đùa tí! Nhưng chị thích em là thật',
     btnReply: 'Em đoán được chị sắp nói gì rồi phải không?',
     reply: 'Vừa giỏi giang, vừa đáng tin, vừa sâu sắc, vừa có 1 chút nổi loạn, và nhiều phần đáng iu',
     mess: 'Chị ko ngại đợi, nhưng ko biết phải đợi đến bao giờ để được gặp em, cũng ko muốn bỏ lỡ cơ hội',
