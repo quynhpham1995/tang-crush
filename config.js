5e8b0a15446ed1eb2a5be18b9826f057d22ed5dd
quynhpham1995 <90567919+quynhpham1995@users.noreply.github.com>
2021-09-16 20:00:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
-    introTitle: 'Em bé Unicorn ơi! =))',
-    introDesc: `Gặp gỡ nhau muộn chút vẫn tốt chứ sao.
+    introTitle: 'Em bé Unicorn ơi!!!',
+    introDesc: `Gặp gỡ nhau muộn một chút vẫn tốt chứ sao.
     Chị đúng độ vào quãng đời chín chắn.
     Còn em thì bước sang tuổi đủ dịu dàng`,
-    btnIntro: 'You ready? =))',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Tất nhiên rồi',
-    btnNo: 'Quìn mơ đấy à...',
-    question:'Thế giới có hơn 7 tỉ người mà sao em lại nhìn trúng chị thế <3',
-    btnReply: 'Gửi lý do cho chị biết vớiiii',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <3',
-    mess: 'Chị biết mà 🥰 Nếu ko phải em thì sẽ là ai được đây',
-    messDesc: 'Would you date me after social distancing?',
-    btnAccept: 'Em chờ Quìn hỏi mãiii',
-    messLink: 'https://www.facebook.com/mirandaxuanquynh/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    btnIntro: 'Chị muốn nói với em là...',
+    title: 'Em ơi, yêu là không lo mà đã lo là không yêu',
+    desc: 'Thế mình yêu nhau đi và đừng lo gì cả nhé',
+    btnYes: 'Em vẫn đợi Quỳnh hỏi đấy',
+    btnNo: 'Quỳnh mơ đấy à',
+    question:'Đùa tí. Nhưng chị thích em là thật',
+    btnReply: 'Em đoán được chị sắp nói gì rồi phải không?',
+    reply: 'Vừa giỏi giang, vừa đáng tin, vừa sâu sắc, vừa có 1 chút nổi loạn, và nhiều phần đáng iu',
+    mess: 'Chị ko ngại đợi, nhưng ko biết phải đợi đến bao giờ để được gặp em, cũng ko muốn bỏ lỡ cơ hội',
+    messDesc: 'Nếu ko phải em thì sẽ là ai được đây',
+    btnAccept: 'Thảo Vy, em có muốn hẹn hò cùng chị không? Nói chị nghe với nhé',
+    messLink: 'https://www.instagram.com/mcquinn9395/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
